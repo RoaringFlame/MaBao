f7dce406228385dfdf8daa357c0426b5d85ad460
makaiqin <1245095397@qq.com>
2016-06-30 17:39:24 +0800

    GoodsVO修改。

--- src/main/java/com/mabao/controller/vo/GoodsVO.java
+++ src/main/java/com/mabao/controller/vo/GoodsVO.java
@@ -5,10 +5,25 @@ package com.mabao.controller.vo;
  * Created by makaiqin 2016/6/30.
  */
 public class GoodsVO {
+    private int id;//商品id
+    private String title;//商品标题
     private String picture;//宝物图片
     private int price;//宝物价格
     private String newDegree;//宝物新旧程度
     private String brand;//宝物品牌
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
     public String getPicture() {
         return picture;
     }
