82d36e7539544b6ec630f109597430c4a5ae0965
Heng Chen <chenheng120@126.com>
2016-07-08 01:32:24 +0800

    添加SpringSecurity模块为项目提供用户及权限管理等功能，增加Tester内存用户供测试使用。

---
+++ src/main/java/com/mabao/config/MethodSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.mabao.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity
+public class MethodSecurityConfig {
+}
---
+++ src/main/java/com/mabao/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.mabao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+  }
+  
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .inMemoryAuthentication()
+        .withUser("Tester").password("password").roles("USER");
+  }
+
+  
+}
