636bcc9445f1422d7f0d28cc6f87f06fd13585af
makaiqin <1245095397@qq.com>
2016-07-12 10:05:48 +0800

    未完成版本

---
+++ src/main/webapp/WEB-INF/views/index_lm.jsp
@@ -0,0 +1,164 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <meta name="viewport"
+          content="maximum-scale=1.0,minimum-scale=1.0,user-scalable=no,width=device-width,initial-scale=1.0"/>
+    <!-- 禁止将数字变为电话号码 -->
+    <meta name="format-detection" content="telephone=no"/>
+    <!-- iphone设备中的safari私有meta标签,允许全屏模式浏览，隐藏浏览器导航栏 -->
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <!-- iphone的私有标签,它指定的iphone中safari顶端的状态条的样式 -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/master.css">
+    <link rel="stylesheet" href="../../css/module.css">
+    <link rel="stylesheet" href="../../script/lib/iscroll/iscroll.css">
+    <script src="../../script/lib/jquery.1.10.2.js"></script>
+    <script src="../../script/lib/bootstrap/bootstrap.min.js"></script>
+    <script src="../../script/lib/iscroll/iscroll.js"></script>
+    <script src="../../script/index.js"></script>
+</head>
+
+<body>
+<div class="content-index">
+    <!--标题-->
+    <header>
+        首页
+        <!--操作按钮-->
+        <div class="header-box">
+            <a href="login.html">
+                <button class="header-left">
+                    <i class="icon icon-return"></i>
+                    登陆
+                </button>
+            </a>
+        </div>
+        <!--操作按钮 END-->
+    </header>
+    <!--标题 END-->
+
+    <!--搜索框-->
+    <div id="searchBox" class="search">
+        <div class="column">
+            <i class="icon icon-dire"></i>
+        </div>
+        <div class="search-input">
+            <button id="btnSearch">
+                <i class="icon icon-search"></i>
+            </button>
+            <input title="请输入关键字">
+        </div>
+    </div>
+    <!--搜索框 END-->
+    <!-- 侧导航栏 -->
+    <div class="sidebar hide" id="sidebar">
+        <ul>
+        </ul>
+    </div>
+
+    <div class="iscroll-wrapper" style="top:48px;bottom: -12px;">
+        <div class="iscroll-scroller">
+            <!--轮播图-->
+            <div id="myCarousel" class="carousel slide">
+                <!-- 轮播（Carousel）指标 -->
+                <ol class="carousel-indicators">
+                </ol>
+                <!-- 轮播（Carousel）项目 -->
+                <div class="carousel-inner">
+                </div>
+                <!-- 轮播（Carousel）导航 -->
+                <a class="carousel-control left" href="#myCarousel"
+                   data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#myCarousel"
+                   data-slide="next">&rsaquo;</a>
+            </div>
+            <!--轮播图 END-->
+
+            <ul id="hideGoods" style="display: none;">
+                <!--部品列表-->
+                <li>
+                    <img src="../img/main-new1.png" alt="">
+                    <div>
+                        <p>
+                            <span>￥30.00</span>
+                            <span>九成新</span>
+                        </p>
+                        <p>H&M&nbsp;CARDIGN</p>
+                        <p>纯棉轻薄，无刺激，适合宝宝娇嫩肌肤</p>
+                    </div>
+                </li>
+                <!--部品列表 END-->
+            </ul>
+            <!--商品-->
+            <div id="newGoods" class="goods">
+                <!-- 功能切换-->
+                <div class="scroll-menu">
+                    <ul>
+                        <li><a class="focus" href="#">新品</a></li>
+                        <li><a href="#">猜你喜欢</a></li>
+                    </ul>
+                </div>
+                <!-- 功能切换 END-->
+                <ul id="newGoodsList" class="goods-list clearfix">
+                </ul>
+                <ul id="likeGoodsList" class="goods-list clearfix">
+                </ul>
+                <!-- 猜你喜欢 -->
+                <form id="likeForm" style="height: 370px;">
+                    <div class="you-like">
+                        <p>请填写以下信息</p>
+                        <ul>
+                            <li>
+                                <label>宝宝姓名：</label>
+                                <label><input name="babyName" type="text" id="username"></label>
+                            </li>
+                            <li>
+                                <label>宝宝生日：</label>
+                                <label><input name="babyDate" type="date"></label>
+                            </li>
+
+                            <li>
+                                <label>宝宝性别：</label>
+                                <label>
+                                    <select name="sex" id="sex"></select>
+                                </label>
+                            </li>
+
+                            <li>
+                                <label>宝宝爱好：</label>
+                                <label><input name="hobby" type="text"></label>
+                            </li>
+                        </ul>
+                        <button id="btnLikeSubmit" class="like-up-button" type="submit">提交</button>
+                    </div>
+                </form>
+                <!-- 猜你喜欢END -->
+            </div>
+        </div>
+    </div>
+
+    <!--底部导航-->
+    <div class="menu" id="menu">
+        <a href="#">
+            <i class="icon-menu icon-menu1"><img src="../../img/footer-c1.png" alt=""></i>
+        </a>
+        <a href="#">
+            <i class="icon-menu icon-menu2"><img src="../../img/footer-2.png" alt=""></i>
+        </a>
+        <a href="#">
+            <i class="icon-menu icon-menu3"><img src="../../img/footer-3.png" alt=""></i>
+        </a>
+        <a href="#">
+            <i class="icon-menu icon-menu4"><img src="../../img/footer-4.png" alt=""></i>
+        </a>
+        <a href="#">
+            <i class="icon-menu icon-menu5"><img src="../../img/footer-5.png" alt=""></i>
+        </a>
+    </div>
+    <!--底部导航 END-->
+</div>
+</body>
+</html>
--- src/main/webapp/script/index.js
+++ src/main/webapp/script/index.js
@@ -1,7 +1,8 @@
 "use strict";
-$(function(){
-    var currentPageNew=0;
-    var currentPageLike=0;
+$(function () {
+
+    var currentPageNew=1;
+    var currentPageLike=1;
     var pageSize=4;
     var searchKey="";
     var goodsTypeId="";
@@ -9,98 +10,147 @@ $(function(){
     var upOrDown="";
     var babyId=null;
     var isNew=true;     //是否当前展示的是新品
-    function initGoodsTypeAndCarousel(){
+
+    //1.首页信息初始化
+    function initIndexPage(){
         $.get("/home",{},function(data){
-            //初始化轮播
-            var smallBanner= data.smallBanner;
-            var myCarousel=$("#myCarousel");
-            $(smallBanner).each(function(index,banner){
-                var li=$("<li></li>")
-                    .attr("data-target","#myCarousel")
-                    .attr("data-slide-to",index);
-                var img=$("<div></div>").addClass("item").append(
-                    $("<img>")
-                        .attr("src","../upload/"+banner.picture)
-                        .attr("alt",banner.alt)
-                        .click(function(){
-                            window.location="/goods/goodsDetail?goodsId="+banner.goodsId;
-                        })
-                );
-                if(index==0){
-                    li.addClass("active");
-                    img.addClass("active");
-                }
-                myCarousel.find(".carousel-indicators").append(li);
-                myCarousel.find(".carousel-inner").append(img);
-            });
-            //初始化类型列表
+            console.log(data);
+
+            //1.侧边栏的初始化
             var typeSidebar=$("#sidebar");
             $("#searchBox").find("div.column").click(function(){
                 typeSidebar.toggleClass("hide");
             });
             var typeList=data.goodsTypeList;
             $(typeList).each(function(index,goodsType){
-                var li=$("<li></li>").append(
-                    $("<a></a>")
-                        .attr("href","#")
+                var li=$($("<li></li>")
                         .text(goodsType.value)
                 ).click(function(){
-                        goodsTypeId=goodsType.key;
-                        loadNewGoods();
+                    if(index>0){
+                    goodsTypeId=goodsType.key;
+                    loadNewGoods();
+                    }
                 });
                 typeSidebar.find("ul").append(li);
             });
-            //初始化搜索文本框
+
+            //2.搜索框的初始化
             $("#btnSearch").click(function(){
                 searchKey=$(this).next("input").val();
                 loadNewGoods();
             });
-            //初始化宝宝信息
+
+            //3.轮播的初始化
+            var smallBanner=data.smallBanner;//获取轮播图片集
+            var myCarousel=$("#myCarousel");//找到jsp页面对应id为myCarousel的项
+            //遍历获取到的轮播图片集,index为索引（从0开始），banner自己定义的名称用来取smallBanner中的值
+            $(smallBanner).each(function(index,banner){
+                var li=$("<li></li>")//添加li标签并为其添加属性值
+                    .attr("data-target","#myCarousel")
+                    .attr("data-slide-to",index)
+                var img=$("<div></div>").addClass("item")//在div中添加一些属性
+                    .append($("<img>")
+                    .attr("src","../upload/"+banner.picture)//添加图片
+                    .attr("alt",banner.alt)//添加提示信息
+                    .click(function(){//点击图片跳转到商品详情
+                        window.location="/goods/goodsDetail?goodsId="+banner.id;
+                    })
+                    );
+                if(index==0){
+                    //索引为0的轮播图设0为主轮播图
+                    li.addClass("active");
+                    img.addClass("active");
+                }
+                //将li标签信息添加到轮播导航中
+                myCarousel.find(".carousel-indicators").append(li);
+                //将img标签信息添加到论波栏目中
+                myCarousel.find(".carousel-inner").append(img);
+            });
+
+            //猜你喜欢，初始化宝宝信息
             if(data.baby){
                 babyId=data.baby.id;
                 babyId=null;
             }
-            //初始化宝宝性别
+
+            //猜你喜欢，初始化宝宝性别
             var genderList=data.gender;
             var genderSelector=$("#sex");
             $(genderList).each(function(index,gender){
                 var option=$("<option></option>").val(gender.key).text(gender.value);
                 genderSelector.append(option);
             });
+
+            //加载新品和猜你喜欢
             initGoods();
+
         },"json");
     }
 
+    //2.初始化新品和猜你喜欢的切换
     function initGoods(){
-        //初始化新品和猜你喜欢的切换
-        $("#newGoodsList").show();
-        $("#likeGoodsList").hide();
+        //$("#likeForm").hide();//猜你喜欢表单的隐藏
+        $("#newGoodsList").show();//新品页初始化为显示
+        $("#likeGoodsList").hide();//猜你喜欢页初始化为隐藏
         var newGoods=$("#newGoods");
-        newGoods.find("div.scroll-menu ul li:eq(0)").click(function(){
-            $(this).find("a").addClass("focus");
-            $(this).next("li").find("a").removeClass("focus");
-            $("#newGoodsList").show();
-            $("#likeGoodsList").hide();
-            isNew=true;
+        newGoods.find("div.scroll-menu ul li:eq(0)").click(function(){//找到div对应的scroll-menu下的ul得第一个li
+            $(this).addClass("focus");//点击新品时给新品加红色下划线
+            $(this).next("li").removeClass("focus");//猜你喜欢下无红色下划线
+            $("#newGoodsList").show();//点击新品时新品列表的显示
+            $("#likeGoodsList").hide();//猜你喜欢列表的隐藏
+            isNew=true;//是否为新品 设为真
         });
         newGoods.find("div.scroll-menu ul li:eq(1)").click(function(){
-            $(this).find("a").addClass("focus");
-            $(this).prev("li").find("a").removeClass("focus");
-            $("#newGoodsList").hide();
-            $("#likeGoodsList").show();
-            isNew=false;
+            $(this).addClass("focus");//点击猜你喜欢时给新品加红色下划线
+            $(this).prev("li").removeClass("focus");//新品无下划线
+            $("#newGoodsList").hide();//点击猜你喜欢列表的显示
+            $("#likeGoodsList").show();//新品的隐藏
+            isNew=false;//是否为新品  设为假
         });
         //加载新品
         loadNewGoods();
         //加载猜你喜欢
         loadLikeGoods();
     }
-    //加载猜你喜欢
+
+    //3.加载新品数据集
+    function loadNewGoods(){
+        $("#likeForm").show();//猜你喜欢表单的隐藏
+        var params={
+            page:currentPageNew,//新品当前页面数
+            pageSize:pageSize,//新品每页展示的数据信息
+            searchKey:searchKey,//搜索关键字
+            goodsTypeId:goodsTypeId//宝物对应的id
+        };
+        var backGoods=$("#hideGoods").find("li");//查找到新品列表下的li标签
+        var newGoodsBox=$("#newGoodsList");//新品展示
+        $.get("/home/goodsSearch",{params:params},function(data){
+            //打印从后台取出的数据信息
+            console.log(data);
+            var goodsList=data.items;
+            $(goodsList).each(function(index,goods){//对新品进行遍历
+                var newGoods=backGoods.clone();//克隆信息
+                newGoods.find("img").attr("src","../upload/"+goods.picture)
+                    .click(function(){//点击图片跳转到商品详情
+                        window.location="/goods/goodsDetail?goodsId="+goods.id;
+                    });
+                newGoods.find("div>p:eq(0)>span:eq(0)").text("￥"+goods.price);
+                newGoods.find("div>p:eq(0)>span:eq(1)").text(goods.newDegree);
+                newGoods.find("div>p:eq(1)").text(goods.brand);
+                newGoods.find("div>p:eq(2)").text(goods.title);
+                newGoodsBox.append(newGoods);
+            });
+            //initScroll();
+        },"json");
+    }
+
+
+    //4.加载猜你喜欢
     function loadLikeGoods(){
-        if(babyId){
+        if(babyId){//如果宝宝id是存在
+            //$("#likeForm").hide();//猜你喜欢表单的隐藏
             var backGoods=$("#hideGoods").find("li");
             var likeGoodsBox=$("#likeGoodsList");
-            $("#likeForm").hide();
             likeGoodsBox.show();
             var params={
                 page:currentPageLike,
@@ -117,79 +167,35 @@ $(function(){
                     likeGoods.find("div>p:eq(2)").text(goods.title);
                     likeGoodsBox.append(likeGoods);
                 });
-                initScroll();
             });
         }
         else{
-            $("#likeForm").show();
+            //宝宝信息不存在点击猜你喜欢时显示表单页面
+            $("#newGoods").find("li:eq(1)").click(function(){
+                $("#likeForm").show();
+            })
             $("#likeGoodsList").hide();
         }
     }
-    //加载新品数据集
-    function loadNewGoods(){
-        var params={
-            page:currentPageNew,
-            pageSize:pageSize,
-            searchKey:searchKey,
-            goodsTypeId:goodsTypeId
-        };
-        var backGoods=$("#hideGoods").find("li");
-        var newGoodsBox=$("#newGoodsList");
-        $.get("/home/goodsSearch",{params:params},function(data){
-            var goodsList=data.items;
-            $(goodsList).each(function(index,goods){
-                var newGoods=backGoods.clone();
-                newGoods.find("img").attr("src","../upload/"+goods.picture);
-                newGoods.find("div>p:eq(0)>span:eq(0)").text("￥"+goods.price);
-                newGoods.find("div>p:eq(0)>span:eq(1)").text(goods.newDegree);
-                newGoods.find("div>p:eq(1)").text(goods.brand);
-                newGoods.find("div>p:eq(2)").text(goods.title);
-                newGoodsBox.append(newGoods);
-            });
-            initScroll();
-        },"json");
-    }
-    function initScroll(){
-        if(myScroll){
-            myScroll.refresh();
-        }
-        else{
-            //拉动刷新
-            myScroll = new IScroll('div.iscroll-wrapper');
-            myScroll.on("scrollEnd",function(){
-                var wrapperHeight=$('div.iscroll-wrapper').height();
-                if(myScroll.y-wrapperHeight<myScroll.maxScrollY){
-                    if(isNew){
-                        currentPageNew++;
-                        loadNewGoods();
-                    }
-                    else{
-                        if(babyId){
-                            currentPageLike++;
-                            loadLikeGoods();
-                        }
-                        else{
-                            console.log(112);
-                        }
-                    }
-                }
-                //console.log(currentPageNew+"------"+myScroll.y+"-"+myScroll.absStartY+"-"+myScroll.distY+"-"+myScroll.maxScrollY);
-            });
-            document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-        }
-    }
 
+    //猜你喜欢表单初始化
     function initFormAction(){
         $("#btnLikeSubmit").click(function () {
             var likeForm=$("#likeForm");
             var babyName=likeForm.find("input[name='babyName']").val();
             var babyDate=likeForm.find("input[name='babyDate']").val();
-            var babyName=likeForm.find("input[name='babyName']").val();
+            var babySex=likeForm.find("input[name='babySex']").val();
             var hobby=likeForm.find("input[name='hobby']").val();
         });
     }
+
+
+    //5.初始化函数
     function init(){
-        initGoodsTypeAndCarousel();
+        initIndexPage();
     }
+
+    //6.调用初始化函数
     init();
+
 });
