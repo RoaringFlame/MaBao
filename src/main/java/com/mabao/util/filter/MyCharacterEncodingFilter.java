b72985d37ce64ab168ff8f2a488127282261118c
Heng Chen <chenheng120@126.com>
2016-07-19 12:39:12 +0800

    添加字符编码过滤器，修复表单传值乱码问题

---
+++ src/main/java/com/mabao/util/filter/MyCharacterEncodingFilter.java
@@ -0,0 +1,12 @@
+package com.mabao.util.filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+public class MyCharacterEncodingFilter extends CharacterEncodingFilter {
+    private final String encoding = "UTF-8";
+
+    public MyCharacterEncodingFilter(){
+        super();
+        super.setEncoding(this.encoding);
+    }
+}
