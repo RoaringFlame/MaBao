03c5b9af619d6dbe30749e5e7fdbd7b23742c1a4
Emma <1245095397@qq.com>
2016-07-27 10:55:04 +0800

    selfup。js校验的完善！

--- src/main/webapp/script/selfup.js
+++ src/main/webapp/script/selfup.js
@@ -10,22 +10,25 @@ $(function () {
             $('#textShow').text(msg).fadeIn(1000).delay(2000).fadeOut(1000);
         }
 
-        //数据校验
+    //数据校验
     function checkInput() {
         var oldPrice = $("#oldPrice").val();
         var newPrice = $("#newPrice").val();
         var upTime = $("#test_default").val();
+        var fileUpload = $("#fileUpload").val();
         var z= /^[0-9]*$/;
-        if(upTime==""&&oldPrice==""&&newPrice==""){
-            showMsg("请将所有转让信息填写完整！");
-        }else if(z.test(oldPrice)&&z.test(newPrice)&&oldPrice!==""&&newPrice!==""){
-            if(upTime!=="") {
-                $(".transfer-form").submit();
-            }else{
-                showMsg("时间栏不能为空");
+        if(fileUpload==null||fileUpload==""){
+            showMsg("请上传完整的宝贝图片！");
+        }else {
+            if (z.test(oldPrice) && z.test(newPrice) && oldPrice !== "" && newPrice !== "") {
+                if (upTime !== "") {
+                    $(".transfer-form").submit();
+                } else {
+                    showMsg("时间栏不能为空");
+                }
+            } else {
+                showMsg("价格栏必须是数字且不能为空");
             }
-        }else{
-            showMsg("价格栏必须是数字且不能为空");
         }
     }
 
